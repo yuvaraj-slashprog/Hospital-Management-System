cc444269afac60eea9c0a4e45ca4caec8cf6124a
Yuvaraj Murugan <yuvaraj@slashprog.com>
2015-11-04 12:14:40 +0530

    hms_db_config.php
    This is data base configuration of my HMS project.

---
+++ hms_db_config.php
@@ -0,0 +1,27 @@
+# Hospital-Management-System
+Here I create HMS using PHP and MySQL
+
+
+
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'hms');
+
+/*class DB_con {
+	public $mysqli1;
+    function __construct() {
+       $this->mysqli1 = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+		
+		if(mysqli_connect_errno()) {
+ 
+			echo "Error: Could not connect to database.";
+ 
+		exit;
+ 
+		}
+    }
+   
+}*/
+?>
